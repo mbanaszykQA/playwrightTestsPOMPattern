f59428ec007bc5cc54550afab18415e06449b6e3
mbanaszykQA <michal.banaszyk.qa@gmail.com>
2025-12-13 15:40:17 +0100

    Update happyPath.spec.ts

--- project/tests/happyPath.spec.ts
+++ project/tests/happyPath.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pom-pages/LoginPage';
+import { InventoryPage } from '../pom-pages/InventoryPage';
 import { CartPage } from '../pom-pages/CartPage';
 import { CheckoutPage } from '../pom-pages/CheckoutPage';
 
-test('Full purchase flow on SauceDemo using POM pattern', async ({ page }) => {
+test('Full purchase flow on SauceDemo using Builder pattern', async ({ page }) => {
   const login = new LoginPage(page);
+  const inventory = new InventoryPage(page);
   const cart = new CartPage(page);
   const checkout = new CheckoutPage(page);
 
@@ -13,7 +15,12 @@ test('Full purchase flow on SauceDemo using POM pattern', async ({ page }) => {
 
   await expect(login.inventoryHeader()).toHaveText('Products');
 
-  await cart.goToCheckout();
+  await inventory.addProductToCart('Sauce Labs Backpack');
+  await inventory.goToCart();
+
+  await expect(cart.cartItem('Sauce Labs Backpack')).toBeVisible();
+
+  await cart.proceedToCheckout();
 
   await checkout.fillForm('Jan', 'Kowalski', '00-001');
   await checkout.finish();

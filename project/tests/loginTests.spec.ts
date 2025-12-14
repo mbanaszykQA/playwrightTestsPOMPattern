64bdd509a861e5de6e62f1107acc3d32875a1b3b
mbanaszykQA <michal.banaszyk.qa@gmail.com>
2025-12-14 20:33:30 +0100

    Create loginTests.spec.ts

---
+++ project/tests/loginTests.spec.ts
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pom-pages/LoginPage';
+
+test.describe('Login tests', () => {
+
+  test('Prawidłowe logowanie - standard_user', async ({ page }) => {
+    const login = new LoginPage(page);
+
+    await login.open();
+    await login.login('standard_user', 'secret_sauce');
+
+    await expect(login.inventoryHeader()).toHaveText('Products');
+  });
+
+  test('Nieprawidłowe logowanie - locked_out_user', async ({ page }) => {
+    const login = new LoginPage(page);
+
+    await login.open();
+    await login.login('locked_out_user', 'secret_sauce');
+
+    const errorMessage = page.locator('[data-test="error"]');
+
+    await expect(errorMessage).toBeVisible();
+    await expect(errorMessage).toHaveText(
+      'Epic sadface: Sorry, this user has been locked out.'
+    );
+  });
+
+});
